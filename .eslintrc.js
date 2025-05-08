f6b64ab541d3668b1e5237d3c620a5c2c471c1be
SAI TEJA SAGARI <saitejasagari2002@gmail.com>
2025-05-08 14:52:17 +0530

    2nd push

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,35 @@ module.exports = {
   },
   rules: {
     'import/extensions': ['error', { js: 'always' }], // require js file extensions in imports
-    'linebreak-style': ['error', 'unix'], // enforce unix linebreaks
+    'linebreak-style': 'off', // enforce unix linebreaks
     'no-param-reassign': [2, { props: false }], // allow modifying properties of param
+    'comma-spacing' : 'off',
+    'indent' : 'off',
+    'eol-last' : 'off',
+    'no-trailing-spaces' : 'off',
+    'object-curly-newline' : 'off',
+    'space-infix-ops' : 'off',
+    'keyword-spacing' : 'off',
+    'padded-blocks' : 'off',
+    'spaced-comment' : 'off',
+    'no-multiple-empty-lines' : 'off',
+    'space-before-blocks' : 'off',
+    'arrow-parens' : 'off',
+    'comma-dangle' : 'off',
+    'no-multiple-empty-lines' : 'off',
+    'no-inner-declarations' : 'off',
+    'brace-style' : 'off',
+    'no-undef' : 'off',
+    'prefer-const' : 'off',
+    'no-shadow' : 'off',
+    'semi' : 'off',
+    'eqeqeq' : 'off',
+    'operator-linebreak' : 'off',
+    'key-spacing' : 'off',
+    'quote-props' : 'off',
+    'no-plusplus' : 'off',
+    'no-dupe-keys' : 'off',
+    'no-console' : 'off',
+    'quotes' : 'off',
   },
 };
